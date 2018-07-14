7c9b88341b5dc9bc04fcebe139e2af0a64c9f0a0
hiena95 <gg2059@naver.com>
2018-07-14 14:21:52 +0900

    정환쓰

--- app/src/main/java/com/example/invisblemirror/MainActivity.java
+++ app/src/main/java/com/example/invisblemirror/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+//454569
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
